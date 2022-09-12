c2e7a0eb66dacf4cfcb623fa8affbf1f67c26d8c
emmanuelboslemayi <hyperfacenzo@gmail.com>
2022-09-12 13:11:28 +0000

    printlist

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
+def safe_print_list(my_list=[], x=0):
     num_elements_printed = 0
     for index in range(int(x)):
         try:
+            print("{:d}".format(my_list[index]), end="")
+            num_elements_printed +=1
+        except (ValueError ,TypeError):
+            pass
+     print()
+     return num_elements_printed
+
+
+
+
+
+
+
