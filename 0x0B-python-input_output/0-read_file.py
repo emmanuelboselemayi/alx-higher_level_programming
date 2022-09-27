ee89e5d600dbf1d66816fdb138c179f0749040a7
emmanuelboslemayi <hyperfacenzo@gmail.com>
2022-09-27 18:31:57 +0000

    0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""0x0B. Python - Input/Output, task 0. Read file """
+
+
+def read_file(filename=""):
+	"""Reads contents of a text file and print to stdout.
+
+	Args:
+		filename (str): name of file to opened
+
+
+	"""
+	with open(filename , encoding ='utf-8) as file:
+		print(file.read(), end=' ')
+
+
